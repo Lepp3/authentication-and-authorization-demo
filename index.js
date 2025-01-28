c980f16885481c2557e39c87319b0db3782854f5
Lepp3 <lubaka05@gmail.com>
2025-01-28 21:55:26 +0200

    express setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('It works');
+    
+});
+
+
+app.listen(5001,()=> console.log('Server is listening on http:/localhost:5001....'));
