70f94fbe6812ed5ccbaa3bd27b6d3145206cc97c
lmilanov <lubaka05@gmail.com>
2025-01-31 17:38:27 +0200

    two ways of setting cookies manually

--- index.js
+++ index.js
@@ -8,5 +8,28 @@ app.get('/',(req,res)=>{
     
 });
 
+// set cookie
+app.get('/set-cookie-manually', (req,res)=>{
 
-app.listen(5001,()=> console.log('Server is listening on http:/localhost:5001....'));
+    //http approach
+    // res.writeHead(200,{
+    //     'Set-Cookie': 'username=lubaka',
+    // });
+
+    //express approach
+    res.setHeader('Set-Cookie', 'username=lyubaka2');
+
+
+    res.end();
+});
+
+//read cookie
+app.get('/get-cookie', (req,res)=>{
+    const cookie = req.header('Cookie');
+
+    console.log(cookie);
+    res.end();
+})
+
+
+app.listen(5001,()=> console.log('Server is listening on http://localhost:5001....'));
