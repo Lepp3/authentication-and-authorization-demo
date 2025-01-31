5c7ea7af41bcde583e5139622ca7a1f4c801a07b
lmilanov <lubaka05@gmail.com>
2025-01-31 18:26:05 +0200

    session management with library

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
 
 const app = express();
 
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'THATSMYSECRET',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false},
+}))
 
 app.get('/',(req,res)=>{
     res.send('It works');
@@ -25,8 +32,8 @@ app.get('/set-cookie-manually', (req,res)=>{
     res.end();
 });
 
-//read cookie
-app.get('/get-cookie', (req,res)=>{
+//get cookie manually
+app.get('/get-cookie-manually', (req,res)=>{
     const cookie = req.header('Cookie');
 
     console.log(cookie);
@@ -37,6 +44,28 @@ app.get('/get-cookie', (req,res)=>{
 app.get('/set-cookie',(req,res)=>{
     res.cookie('username','lyubaka3');
     res.end();
+});
+
+//get cookie with library
+app.get('/get-cookie', (req,res)=>{
+    const cookie = req.cookies['username'];
+    console.log(cookie);
+    res.end();
+});
+
+//session demo
+app.get('/set-session-data/:name', (req,res)=>{
+    req.session.name = req.params.name;
+    req.session.age = 20;
+
+
+    res.end();
+});
+
+//get session data
+app.get('/get-session-data',(req,res)=>{
+    console.log(req.session);
+    res.send(req.session.name);
 })
 
 
